1d059afd2ce2b61eb33ee08cd61c29a9b86e4fe8
Luke <mackenzieluke94@gmail.com>
2019-06-18 20:39:55 -0400

    i forget

---
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const navigationItem = props => (
+  <li>
+    <a href="">HEY</a>
+  </li>
+);
+
+export default navigationItem;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavigationItem from "../NavigationItem/NavigationItem";
+
+const navigationItems = () => (
+  <ul>
+    <h1>HEY</h1>
+  </ul>
+);
+
+export default navigationItems;
